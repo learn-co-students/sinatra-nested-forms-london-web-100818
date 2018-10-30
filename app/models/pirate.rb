858c45734e800a56f3484214f87d95b3fb64f247
github username <slonanuga@gmail.com>
2018-10-30 21:41:49 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+@@all = []
+attr_accessor :name, :weight, :height
+
+    def initialize(pirate)
+        @name =    pirate[:name]
+        @weight =  pirate[:weight]
+        @height =  pirate[:height]
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+
+@@all = []
+attr_accessor :name, :type, :booty
+
+    def initialize(ship)
+        @name = ship[:name]
+        @type = ship[:type]
+        @booty = ship[:booty]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
+
 end
